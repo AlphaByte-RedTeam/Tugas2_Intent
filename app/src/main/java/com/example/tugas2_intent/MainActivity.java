2627c430ab495184d2e5b49e26ab54885d0f96dc
Janice Claresta Lingga <andrew.avv03@gmail.com>
2022-09-21 13:12:34 +0700

    Update changes

--- app/src/main/java/com/example/tugas2_intent/MainActivity.java
+++ app/src/main/java/com/example/tugas2_intent/MainActivity.java
@@ -9,33 +9,43 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
-    EditText name = findViewById(R.id.id_name);
-    Button intent = findViewById(R.id.btn_intent);
-    private Intent SecondActivity;
+//    EditText name = findViewById(R.id.id_name);
+//    Button intent = findViewById(R.id.btn_intent);
+//    private Intent SecondActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("Main Activity");
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Main Activity");
 
-        startActivity(new Intent(this, SecondActivity.class));
+        Button click = findViewById(R.id.btnClickMe);
+        EditText editNameReceive = findViewById(R.id.editName);
+        String users = editNameReceive.getText().toString();
 
-        intent.setOnClickListener(view -> {
-            if (name.length() == 0) {
-                Toast.makeText(getApplication(), "Username belum dimasukkan", Toast.LENGTH_LONG).show();
-            }else {
-                Toast.makeText(getApplication(), "Usernamemu salah", Toast.LENGTH_LONG).show();
-                Toast.makeText(getApplication(), "Haiii...", Toast.LENGTH_LONG).show();
+        click.setOnClickListener(view -> {
+            if (editNameReceive.length() == 0)
+                Toast.makeText(this, "Username belum diinput", Toast.LENGTH_LONG).show();
+            else {
+                Toast.makeText(this, "Haiii...", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(this, SecondActivity.class);
-                String users = name.getText().toString();
                 intent.putExtra("Name", users);
                 startActivity(intent);
-
-                startActivity(SecondActivity);
             }
         });
+
+//        startActivity(new Intent(this, SecondActivity.class));
+//
+//        intent.setOnClickListener(view -> {
+//            if (name.length() == 0) {
+//            }else {
+//
+//                startActivity(SecondActivity);
+//            }
+//        });
     }
 
 }
